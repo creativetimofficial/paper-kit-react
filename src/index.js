7a01f86593566a3afb9d6ab1bf122ef1de28ce63
Manu <emanuelnazare@yahoo.ro>
2019-06-19 14:15:42 +0300

    - adding stuff needed for a kit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/scss/paper-kit.scss";
+import "assets/demo/demo.css";
+// pages
+import Index from "views/Index.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+// others
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={props => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        render={props => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={props => <ProfilePage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={props => <RegisterPage {...props} />}
+      />
+      <Redirect to="/index" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
