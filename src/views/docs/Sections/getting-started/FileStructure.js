39edf52dd0bac2727d6a792fb30bf566aca9b1c4
Manu <emanuelnazare@yahoo.ro>
2019-06-25 12:14:34 +0300

    - getting started done

--- src/views/docs/Sections/getting-started/FileStructure.js
+++ src/views/docs/Sections/getting-started/FileStructure.js
@@ -1,8 +1,77 @@
 import React from "react";
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // reactstrap components
 // import {
 // } from "reactstrap";
+
+const files = `paper-kit-react-master
+.
+├── CHANGELOG.md
+├── ISSUE_TEMPLATE.md
+├── LICENSE.md
+├── README.md
+├── jsconfig.json
+├── package.json
+├── public
+│   ├── favicon.ico
+│   ├── index.html
+│   └── manifest.json
+└── src
+    ├── index.js
+    ├── logo.svg
+    ├── assets
+    │   ├── css
+    │   │   ├── bootstrap.min.css
+    │   │   └── bootstrap.min.css.map
+    │   ├── demo
+    │   │   └── demo.css
+    │   ├── fonts
+    │   ├── img
+    │   │   ├── examples
+    │   │   ├── faces
+    │   │   ├── flags
+    │   └── scss
+    │       ├── paper-kit
+    │       │   ├── cards
+    │       │   ├── mixins
+    │       │   └── plugins
+    │       ├── react
+    │       │   ├── paper-kit
+    │       │   ├── plugins
+    │       │   └── react-differences.scss
+    │       └── paper-kit.scss
+    ├── components
+    │   ├── Footers
+    │   │   └── DemoFooter.js
+    │   ├── Headers
+    │   │   ├── IndexHeader.js
+    │   │   ├── LandingPageHeader.js
+    │   │   └── ProfilePageHeader.js
+    │   └── Navbars
+    │       ├── ExamplesNavbar.js
+    │       └── IndexNavbar.js
+    └── views
+        ├── Index.js
+        ├── NucleoIcons.js
+        ├── examples
+        │   ├── LandingPage.js
+        │   ├── ProfilePage.js
+        │   └── RegisterPage.js
+        └── index-sections
+            ├── SectionButtons.js
+            ├── SectionCarousel.js
+            ├── SectionDark.js
+            ├── SectionDownload.js
+            ├── SectionExamples.js
+            ├── SectionJavaScript.js
+            ├── SectionLogin.js
+            ├── SectionNavbars.js
+            ├── SectionNavigation.js
+            ├── SectionNotifications.js
+            ├── SectionNucleoIcons.js
+            ├── SectionProgress.js
+            └── SectionTypography.js`
 
 class FileStructure extends React.Component {
   render() {
@@ -17,6 +86,9 @@ class FileStructure extends React.Component {
           Once you have downloaded the archive and opened it, you will find the
           following structure:
         </p>
+        <SyntaxHighlighter language="bash" style={prism}>
+          {files}
+        </SyntaxHighlighter>
         <h2 id="restyled-components">Restyled Components</h2>
         <p>
           Here is the list of Bootstrap 4 components that we restyled in Paper
