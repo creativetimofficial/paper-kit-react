53390f9639c35333447ef43e206acbef6f9270b2
Manu <emanuelnazare@yahoo.ro>
2019-06-26 09:31:50 +0300

    - popovers

--- src/views/docs/Sections/restyled-components/Popovers.js
+++ src/views/docs/Sections/restyled-components/Popovers.js
@@ -1,25 +1,215 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
-// reactstrap components
-import {
-  Button,
-  PopoverBody,
-  PopoverHeader,
-  UncontrolledPopover
-} from "reactstrap";
+import { Button, Popover, PopoverHeader, PopoverBody } from "reactstrap";
 
-class Popovers extends React.Component {
+const exampleLiveDemo = `import React from 'react';
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
     return (
       <>
-        <h1 className="bd-title" id="content">
-          Popovers
-        </h1>
+        <Button id="Popover1" onClick={this.toggle} size="lg" color="primary">
+          Click to toggle popover
+        </Button>
+        <Popover
+          placement="right"
+          isOpen={this.state.popoverOpen}
+          target="Popover1"
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover Title</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+      </>
+    );
+  }
+}`;
+
+const exampleFourDirections = `import React from "react";
+
+import { Button, Popover, PopoverHeader, PopoverBody } from "reactstrap";
+
+class Popovers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popover1: false,
+      popover2: false,
+      popover3: false,
+      popover4: false
+    };
+  }
+  toggle(popover) {
+    this.setState({
+      [popover]: !this.state[popover]
+    });
+  }
+  render() {
+    return (
+      <>
+        <Button id="popover1" onClick={() => this.toggle("popover1")}>
+          On left
+        </Button>
+        <Popover
+          placement="left"
+          isOpen={this.state.popover1}
+          target="popover1"
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover On left</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+        <Button id="popover2" onClick={() => this.toggle("popover2")}>
+          On top
+        </Button>
+        <Popover
+          placement="top"
+          isOpen={this.state.popover2}
+          target="popover2"
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover On top</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+        <Button id="popover3" onClick={() => this.toggle("popover3")}>
+          On top
+        </Button>
+        <Popover
+          placement="right"
+          isOpen={this.state.popover3}
+          target="popover3"
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover On right</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+        <Button id="popover4" onClick={() => this.toggle("popover4")}>
+          On top
+        </Button>
+        <Popover
+          placement="bottom"
+          isOpen={this.state.popover4}
+          target="popover4"
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover On bottom</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+      </>
+    );
+  }
+}
+
+export default Popovers;
+`;
+
+const exampleNextClickClose = `import React from 'react';
+import { Button, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Button id="Popover1" onClick={this.toggle} size="lg" color="primary">
+          Dismissible popover
+        </Button>
+        <Popover
+          placement="right"
+          isOpen={this.state.popoverOpen}
+          target="Popover1"
+          toggle={this.toggle}
+          className="popover-primary"
+        >
+          <PopoverHeader>Popover Title</PopoverHeader>
+          <PopoverBody>
+            And here's some amazing content. It's very engaging. Right?
+          </PopoverBody>
+        </Popover>
+      </>
+    );
+  }
+}`;
+
+class Popovers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popoverOpen: false,
+      popover1: false,
+      popover2: false,
+      popover3: false,
+      popover4: false,
+      Popover2: false
+    };
+  }
+  toggle1 = () => {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  };
+  toggle2 = () => {
+    this.setState({
+      Popover2: !this.state.Popover2
+    });
+  };
+  toggle = popover => {
+    this.setState({
+      [popover]: !this.state[popover]
+    });
+  };
+  render() {
+    return (
+      <>
+        <h1 className="bd-title">Popovers</h1>
         <p className="bd-lead">
           Documentation and examples for adding Bootstrap popovers, like those
           found in iOS, to any element on your site.
         </p>
-        <h2 id="static-popover">Static popover</h2>
+        <h2>Static popover</h2>
         <p>Four options are available: top, right, bottom, and left aligned.</p>
         <div className="bd-example bd-example-popover-static">
           <div className="popover bs-popover-top bs-popover-top-docs">
@@ -64,138 +254,133 @@ class Popovers extends React.Component {
           </div>
           <div className="clearfix" />
         </div>
-        <h2 id="live-demo">Live demo</h2>
-        <div className="bd-example" data-example-id="">
+        <h2>Live demo</h2>
+        <div className="bd-example">
           <Button
-            color="danger"
-            data-content="And here's some amazing content. It's very engaging. Right?"
-            id="tooltip958516564"
+            id="Popover1"
+            onClick={this.toggle1}
             size="lg"
-            title="Popover title"
-            type="button"
+            color="primary"
           >
             Click to toggle popover
           </Button>
-          <UncontrolledPopover target="tooltip958516564">
-            <PopoverHeader>Popover title</PopoverHeader>
-            <PopoverBody>
-              And here's some amazing content. It's very engaging. Right?
-            </PopoverBody>
-          </UncontrolledPopover>
-        </div>
-        <h3 id="four-directions">Four directions</h3>
-        <div className="bd-example popover-demo">
-          <div className="bd-example-popovers">
-            <Button
-              color="secondary"
-              data-container="body"
-              data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."
-              data-placement="top"
-              id="tooltip163409787"
-              type="button"
-            >
-              Popover on top
-            </Button>
-            <UncontrolledPopover placement="top" target="tooltip163409787">
-              <PopoverBody>
-                Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
-              </PopoverBody>
-            </UncontrolledPopover>
-            <Button
-              color="secondary"
-              data-container="body"
-              data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."
-              data-placement="right"
-              id="tooltip436498734"
-              type="button"
-            >
-              Popover on right
-            </Button>
-            <UncontrolledPopover placement="right" target="tooltip436498734">
-              <PopoverBody>
-                Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
-              </PopoverBody>
-            </UncontrolledPopover>
-            <Button
-              color="secondary"
-              data-container="body"
-              data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."
-              data-placement="bottom"
-              id="tooltip113461198"
-              type="button"
-            >
-              Popover on bottom
-            </Button>
-            <UncontrolledPopover placement="bottom" target="tooltip113461198">
-              <PopoverBody>
-                Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
-              </PopoverBody>
-            </UncontrolledPopover>
-            <Button
-              color="secondary"
-              data-container="body"
-              data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."
-              data-placement="left"
-              id="tooltip677809118"
-              type="button"
-            >
-              Popover on left
-            </Button>
-            <UncontrolledPopover placement="left" target="tooltip677809118">
-              <PopoverBody>
-                Vivamus sagittis lacus vel augue laoreet rutrum faucibus.
-              </PopoverBody>
-            </UncontrolledPopover>
-          </div>
-        </div>
-        <h3 id="dismiss-on-next-click">Dismiss on next click</h3>
-        <p>
-          Use the <code className="highlighter-rouge">focus</code>
-          trigger to dismiss popovers on the user’s next click of a different
-          element than the toggle element.
-        </p>
-        <div className="bd-callout bd-callout-danger">
-          <h4 id="specific-markup-required-for-dismiss-on-next-click">
-            Specific markup required for dismiss-on-next-click
-          </h4>
-          <p>
-            For proper cross-browser and cross-platform behavior, you must use
-            the <code className="highlighter-rouge">{`<a>`}</code>
-            tag, <em>not</em>
-            the <code className="highlighter-rouge">{`<button>`}</code>
-            tag, and you also must include a{" "}
-            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex">
-              <code className="highlighter-rouge">tabindex</code>
-            </a>
-            attribute.
-          </p>
-        </div>
-        <div className="bd-example" data-example-id="">
-          <Button
-            color="danger"
-            data-content="And here's some amazing content. It's very engaging. Right?"
-            data-trigger="focus"
-            id="tooltip135659297"
-            role="button"
-            size="lg"
-            tabindex="0"
-            title="Dismissible popover"
+          <Popover
+            placement="right"
+            isOpen={this.state.popoverOpen}
+            target="Popover1"
+            className="popover-primary"
           >
-            Dismissible popover
-          </Button>
-          <UncontrolledPopover target="tooltip135659297">
-            <PopoverHeader>Dismissible popover</PopoverHeader>
+            <PopoverHeader>Popover Title</PopoverHeader>
             <PopoverBody>
               And here's some amazing content. It's very engaging. Right?
             </PopoverBody>
-          </UncontrolledPopover>
+          </Popover>
         </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {exampleLiveDemo}
+        </SyntaxHighlighter>
+        <h2>Four directions</h2>
+        <div className="bd-example">
+          <Button id="popover1" onClick={() => this.toggle("popover1")}>
+            On left
+          </Button>
+          <Popover
+            placement="left"
+            isOpen={this.state.popover1}
+            target="popover1"
+            className="popover-primary"
+          >
+            <PopoverHeader>Popover On left</PopoverHeader>
+            <PopoverBody>
+              And here's some amazing content. It's very engaging. Right?
+            </PopoverBody>
+          </Popover>
+          <Button id="popover2" onClick={() => this.toggle("popover2")}>
+            On top
+          </Button>
+          <Popover
+            placement="top"
+            isOpen={this.state.popover2}
+            target="popover2"
+            className="popover-primary"
+          >
+            <PopoverHeader>Popover On top</PopoverHeader>
+            <PopoverBody>
+              And here's some amazing content. It's very engaging. Right?
+            </PopoverBody>
+          </Popover>
+          <Button id="popover3" onClick={() => this.toggle("popover3")}>
+            On top
+          </Button>
+          <Popover
+            placement="right"
+            isOpen={this.state.popover3}
+            target="popover3"
+            className="popover-primary"
+          >
+            <PopoverHeader>Popover On right</PopoverHeader>
+            <PopoverBody>
+              And here's some amazing content. It's very engaging. Right?
+            </PopoverBody>
+          </Popover>
+          <Button id="popover4" onClick={() => this.toggle("popover4")}>
+            On top
+          </Button>
+          <Popover
+            placement="bottom"
+            isOpen={this.state.popover4}
+            target="popover4"
+            className="popover-primary"
+          >
+            <PopoverHeader>Popover On bottom</PopoverHeader>
+            <PopoverBody>
+              And here's some amazing content. It's very engaging. Right?
+            </PopoverBody>
+          </Popover>
+        </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {exampleFourDirections}
+        </SyntaxHighlighter>
+        <h2>Dismiss on next click</h2>
         <p>
-          If you want to see more examples and properties please check the
-          official{" "}
-          <a href="https://getbootstrap.com/docs/4.3/components/popovers/">
-            Bootstrap Documentation
-          </a>
+          You can automatically dissmis the Popover by adding the{" "}
+          <code>toggle</code> prop on it like so:
+        </p>
+        <div className="bd-example">
+          <Button
+            id="Popover2"
+            onClick={this.toggle2}
+            size="lg"
+            color="primary"
+          >
+            Click to toggle popover
+          </Button>
+          <Popover
+            placement="right"
+            isOpen={this.state.Popover2}
+            target="Popover2"
+            toggle={this.toggle2}
+            className="popover-primary"
+          >
+            <PopoverHeader>Popover Title</PopoverHeader>
+            <PopoverBody>
+              And here's some amazing content. It's very engaging. Right?
+            </PopoverBody>
+          </Popover>
+        </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {exampleNextClickClose}
+        </SyntaxHighlighter>
+        <h2>Props</h2>
+        <p>
+          For props please refer to{" "}
+          <a
+            href="https://reactstrap.github.io/components/popovers/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            reactstrap's popover documentation
+          </a>.
         </p>
       </>
     );
