f737532b5b655be2d7805fc69cdd9d5ea051125c
Manu <emanuelnazare@yahoo.ro>
2019-06-26 09:29:58 +0300

    - run prettier

--- src/views/docs/Sections/restyled-components/Navbar.js
+++ src/views/docs/Sections/restyled-components/Navbar.js
@@ -1369,20 +1369,19 @@ function NavbarDocs() {
                       <i
                         aria-hidden={true}
                         className="fa fa-facebook-official"
-                      />
-                        Facebook
+                      />{" "}
+                      Facebook
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink href="#pablo" onClick={e => e.preventDefault()}>
-                      <i aria-hidden={true} className="fa fa-twitter" />
-                        Twitter
+                      <i aria-hidden={true} className="fa fa-twitter" /> Twitter
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink href="#pablo" onClick={e => e.preventDefault()}>
-                      <i aria-hidden={true} className="fa fa-instagram" />
-                        Instagram
+                      <i aria-hidden={true} className="fa fa-instagram" />{" "}
+                      Instagram
                     </NavLink>
                   </NavItem>
                 </Nav>
