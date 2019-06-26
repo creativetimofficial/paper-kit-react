55efb96450a641bca2ed7c2f07dd8b2ef7c1722f
Manu <emanuelnazare@yahoo.ro>
2019-06-26 09:40:06 +0300

    - typography

--- src/views/docs/Sections/restyled-components/Typography.js
+++ src/views/docs/Sections/restyled-components/Typography.js
@@ -1,13 +1,56 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
-// reactstrap components
-// import {
-// } from "reactstrap";
+const codeHeadings = `<h1>h1. Bootstrap heading</h1>
+<h2>h2. Bootstrap heading</h2>
+<h3>h3. Bootstrap heading</h3>
+<h4>h4. Bootstrap heading</h4>
+<h5>h5. Bootstrap heading</h5>
+<h6>h6. Bootstrap heading</h6>`;
 
-class Typography extends React.Component {
+const codeClases = `<p className="h1">h1. Bootstrap heading</p>
+<p className="h2">h2. Bootstrap heading</p>
+<p className="h3">h3. Bootstrap heading</p>
+<p className="h4">h4. Bootstrap heading</p>
+<p className="h5">h5. Bootstrap heading</p>
+<p className="h6">h6. Bootstrap heading</p>`;
+
+const codeTitle = `<h1 className="title">h1. Bootstrap heading</h1>
+<h2 className="title">h2. Bootstrap heading</h2>
+<h3 className="title">h3. Bootstrap heading</h3>
+<h4 className="title">h4. Bootstrap heading</h4>
+<h5 className="title">h5. Bootstrap heading</h5>
+<h6 className="title">h6. Bootstrap heading</h6>`;
+
+const codeSmall = `<h3>
+    Fancy display heading
+    <small className="text-muted">With faded secondary text</small>
+</h3>`;
+
+const codeBlockquote = `<blockquote className="blockquote">
+    <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+</blockquote>`;
+
+const codeSource = `<blockquote className="blockquote">
+  <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>`;
+
+const codeAlignment1 = `<blockquote className="blockquote text-center">
+    <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>`;
+
+const codeAlignment2 = `<blockquote className="blockquote text-right">
+    <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+    <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>`;
+
+class TypographySection extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <h1 className="bd-title" id="content">
           Typography
         </h1>
@@ -15,89 +58,77 @@ class Typography extends React.Component {
           Documentation and examples for Bootstrap typography, including global
           settings, headings, body text, lists, and more.
         </p>
-        <h2 id="headings">Headings</h2>
+        <h2>Headings</h2>
+        <div className="bd-example">
+          <table>
+            <thead>
+              <tr>
+                <td>Heading</td>
+                <td>Example</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <code>{`<h1></h1>`}</code>
+                </td>
+                <td>
+                  <h1>h1. Bootstrap heading</h1>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <code>{`<h2></h2>`}</code>
+                </td>
+                <td>
+                  <h2>h2. Bootstrap heading</h2>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <code>{`<h3></h3>`}</code>
+                </td>
+                <td>
+                  <h3>h3. Bootstrap heading</h3>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <code>{`<h4></h4>`}</code>
+                </td>
+                <td>
+                  <h4>h4. Bootstrap heading</h4>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <code>{`<h5></h5>`}</code>
+                </td>
+                <td>
+                  <h5>h5. Bootstrap heading</h5>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <code>{`<h6></h6>`}</code>
+                </td>
+                <td>
+                  <h6>h6. Bootstrap heading</h6>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeHeadings}
+        </SyntaxHighlighter>
         <p>
-          All HTML headings, <code className="highlighter-rouge">{`<h1>`}</code>
-          through <code className="highlighter-rouge">{`<h6>`}</code>, are
-          available.
+          <code className="highlighter-rouge">.h1</code> through{" "}
+          <code className="highlighter-rouge">.h6</code> classes are also
+          available, for when you want to match the font styling of a heading
+          but cannot use the associated HTML element.
         </p>
-        <table>
-          <thead>
-            <tr>
-              <th>Heading</th>
-              <th>Example</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h1></h1>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h1">h1. Bootstrap heading</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h2></h2>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h2">h2. Bootstrap heading</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h3></h3>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h3">h3. Bootstrap heading</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h4></h4>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h4">h4. Bootstrap heading</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h5></h5>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h5">h5. Bootstrap heading</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p>
-                  <code className="highlighter-rouge">{`<h6></h6>`}</code>
-                </p>
-              </td>
-              <td>
-                <span className="h6">h6. Bootstrap heading</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <p>
-          <code className="highlighter-rouge">.h1</code>
-          through <code className="highlighter-rouge">.h6</code>
-          classes are also available, for when you want to match the font
-          styling of a heading but cannot use the associated HTML element.
-        </p>
-        <div className="bd-example" data-example-id="">
+        <div className="bd-example">
           <p className="h1">h1. Bootstrap heading</p>
           <p className="h2">h2. Bootstrap heading</p>
           <p className="h3">h3. Bootstrap heading</p>
@@ -105,28 +136,49 @@ class Typography extends React.Component {
           <p className="h5">h5. Bootstrap heading</p>
           <p className="h6">h6. Bootstrap heading</p>
         </div>
-        <h3 id="customizing-headings">Customizing headings</h3>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeClases}
+        </SyntaxHighlighter>
+        <p>
+          <code className="highlighter-rouge">.title</code>, for when you want
+          some bolder titles with a different font family.
+        </p>
+        <div className="bd-example">
+          <h1 className="title">h1. Bootstrap heading</h1>
+          <h2 className="title">h2. Bootstrap heading</h2>
+          <h3 className="title">h3. Bootstrap heading</h3>
+          <h4 className="title">h4. Bootstrap heading</h4>
+          <h5 className="title">h5. Bootstrap heading</h5>
+          <h6 className="title">h6. Bootstrap heading</h6>
+        </div>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeTitle}
+        </SyntaxHighlighter>
+        <h3>Customizing headings</h3>
         <p>
           Use the included utility classes to recreate the small secondary
           heading text from Bootstrap 3.
         </p>
         <div className="bd-example">
-          <span className="h3">
-            Fancy display heading{" "}
+          <h3>
+            Fancy display heading
             <small className="text-muted">With faded secondary text</small>
-          </span>
+          </h3>
         </div>
-        <h2 id="blockquotes">Blockquotes</h2>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeSmall}
+        </SyntaxHighlighter>
+        <h2>Blockquotes</h2>
         <p>
           For quoting blocks of content from another source within your
           document. Wrap{" "}
           <code className="highlighter-rouge">
-            {`<blockquote className="blockquote">`}
-          </code>
-          around any <abbr title="HyperText Markup Language">HTML</abbr>
-          as the quote.
+            &lt;blockquote className="blockquote"&gt;
+          </code>{" "}
+          around any <abbr title="HyperText Markup Language">HTML</abbr> as the
+          quote.
         </p>
-        <div className="bd-example" data-example-id="">
+        <div className="bd-example">
           <blockquote className="blockquote">
             <p className="mb-0">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
@@ -134,16 +186,11 @@ class Typography extends React.Component {
             </p>
           </blockquote>
         </div>
-        <h3 id="naming-a-source">Naming a source</h3>
-        <p>
-          Add a{" "}
-          <code className="highlighter-rouge">
-            {`<footer className="blockquote-footer">`}
-          </code>
-          for identifying the source. Wrap the name of the source work in{" "}
-          <code className="highlighter-rouge">{`<cite>`}</code>.
-        </p>
-        <div className="bd-example" data-example-id="">
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeBlockquote}
+        </SyntaxHighlighter>
+        <h3>Naming a source</h3>
+        <div className="bd-example">
           <blockquote className="blockquote">
             <p className="mb-0">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
@@ -154,12 +201,11 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
-        <h3 id="alignment">Alignment</h3>
-        <p>
-          Use text utilities as needed to change the alignment of your
-          blockquote.
-        </p>
-        <div className="bd-example" data-example-id="">
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeSource}
+        </SyntaxHighlighter>
+        <h3>Alignment</h3>
+        <div className="bd-example">
           <blockquote className="blockquote text-center">
             <p className="mb-0">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
@@ -170,7 +216,10 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
-        <div className="bd-example" data-example-id="">
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeAlignment1}
+        </SyntaxHighlighter>
+        <div className="bd-example">
           <blockquote className="blockquote text-right">
             <p className="mb-0">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
@@ -181,16 +230,12 @@ class Typography extends React.Component {
             </footer>
           </blockquote>
         </div>
-        <p>
-          If you want to see more examples and properties please check the
-          official{" "}
-          <a href="https://getbootstrap.com/docs/4.3/content/typography/">
-            Bootstrap Documentation
-          </a>
-        </p>
-      </>
+        <SyntaxHighlighter language="jsx" style={prism}>
+          {codeAlignment2}
+        </SyntaxHighlighter>
+      </div>
     );
   }
 }
 
-export default Typography;
+export default TypographySection;
