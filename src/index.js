020d488cd4b15f99e9465b012e7ad706c1fcafa4
EINazare <emanuelioannazare@gmail.com>
2020-06-11 19:23:00 +0300

    docs: change versions

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
 import "assets/css/bootstrap.min.css";
-import "assets/scss/paper-kit.scss";
-import "assets/demo/demo.css";
+import "assets/scss/paper-kit.scss?v=1.2.0";
+import "assets/demo/demo.css?v=1.2.0";
 // pages
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
