ff7ef9e7a1dc09d6808583928a0b23af6d57c254
EINazare <emanuelioannazare@gmail.com>
2021-03-23 16:11:54 +0200

    fix: solve new required images

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -31,7 +31,7 @@ function IndexHeader() {
         className="page-header section-dark"
         style={{
           backgroundImage:
-            "url(" + require("assets/img/antoine-barres.jpg") + ")",
+            "url(" + require("assets/img/antoine-barres.jpg").default + ")",
         }}
       >
         <div className="filter" />
@@ -40,10 +40,10 @@ function IndexHeader() {
             <div className="title-brand">
               <h1 className="presentation-title">Paper Kit React</h1>
               <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
+                <img alt="..." src={require("assets/img/fog-low.png").default} />
               </div>
               <div className="fog-low right">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
+                <img alt="..." src={require("assets/img/fog-low.png").default} />
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
@@ -54,7 +54,7 @@ function IndexHeader() {
         <div
           className="moving-clouds"
           style={{
-            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+            backgroundImage: "url(" + require("assets/img/clouds.png").default + ")",
           }}
         />
         <h6 className="category category-absolute">
@@ -66,7 +66,7 @@ function IndexHeader() {
             <img
               alt="..."
               className="creative-tim-logo"
-              src={require("assets/img/creative-tim-white-slim2.png")}
+              src={require("assets/img/creative-tim-white-slim2.png").default}
             />
           </a>
         </h6>
