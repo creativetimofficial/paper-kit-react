f69b34a0f905e25956d4ff5a25536752f0bf45ff
EINazare <emanuelioannazare@gmail.com>
2021-03-23 17:30:33 +0200

    refactor: move static bs file to node modules one

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
-import "assets/css/bootstrap.min.css";
+import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 // pages
