d28edf9d9af9f8cffdcc9d36a484a85380c10f46
EINazare <emanuelioannazare@gmail.com>
2021-03-23 16:11:59 +0200

    fix: solve new required images

--- src/views/examples/ProfilePage.js
+++ src/views/examples/ProfilePage.js
@@ -66,7 +66,7 @@ function ProfilePage() {
               <img
                 alt="..."
                 className="img-circle img-no-padding img-responsive"
-                src={require("assets/img/faces/joe-gardner-2.jpg")}
+                src={require("assets/img/faces/joe-gardner-2.jpg").default}
               />
             </div>
             <div className="name">
@@ -129,7 +129,7 @@ function ProfilePage() {
                           <img
                             alt="..."
                             className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/clem-onojeghuo-2.jpg")}
+                            src={require("assets/img/faces/clem-onojeghuo-2.jpg").default}
                           />
                         </Col>
                         <Col className="ml-auto mr-auto" lg="7" md="4" xs="4">
@@ -159,7 +159,7 @@ function ProfilePage() {
                           <img
                             alt="..."
                             className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/ayo-ogunseinde-2.jpg")}
+                            src={require("assets/img/faces/ayo-ogunseinde-2.jpg").default}
                           />
                         </Col>
                         <Col lg="7" md="4" xs="4">
