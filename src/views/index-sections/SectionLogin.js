d4571089b76e8fa24c0d1150b3704ee1d4ea7a9d
EINazare <emanuelioannazare@gmail.com>
2021-03-23 16:16:17 +0200

    style: run prettier

--- src/views/index-sections/SectionLogin.js
+++ src/views/index-sections/SectionLogin.js
@@ -40,7 +40,8 @@ function SectionLogin() {
       <div
         className="section section-image section-login"
         style={{
-          backgroundImage: "url(" + require("assets/img/login-image.jpg").default + ")",
+          backgroundImage:
+            "url(" + require("assets/img/login-image.jpg").default + ")",
         }}
       >
         <Container>
