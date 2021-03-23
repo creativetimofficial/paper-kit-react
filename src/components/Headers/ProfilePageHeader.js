568bfb218dbdbbdc0b4483b443f693f5687b523c
EINazare <emanuelioannazare@gmail.com>
2021-03-23 16:11:56 +0200

    fix: solve new required images

--- src/components/Headers/ProfilePageHeader.js
+++ src/components/Headers/ProfilePageHeader.js
@@ -44,7 +44,7 @@ function ProfilePageHeader() {
       <div
         style={{
           backgroundImage:
-            "url(" + require("assets/img/fabio-mangione.jpg") + ")",
+            "url(" + require("assets/img/fabio-mangione.jpg").default + ")",
         }}
         className="page-header page-header-xs"
         data-parallax={true}
