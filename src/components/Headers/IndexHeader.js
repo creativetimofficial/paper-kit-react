b2c795f2daa4e95e7d8cfef78700fa9a1e2d65c6
EINazare <emanuelioannazare@gmail.com>
2021-03-23 16:16:03 +0200

    style: run prettier

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -40,10 +40,16 @@ function IndexHeader() {
             <div className="title-brand">
               <h1 className="presentation-title">Paper Kit React</h1>
               <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png").default} />
+                <img
+                  alt="..."
+                  src={require("assets/img/fog-low.png").default}
+                />
               </div>
               <div className="fog-low right">
-                <img alt="..." src={require("assets/img/fog-low.png").default} />
+                <img
+                  alt="..."
+                  src={require("assets/img/fog-low.png").default}
+                />
               </div>
             </div>
             <h2 className="presentation-subtitle text-center">
@@ -54,7 +60,8 @@ function IndexHeader() {
         <div
           className="moving-clouds"
           style={{
-            backgroundImage: "url(" + require("assets/img/clouds.png").default + ")",
+            backgroundImage:
+              "url(" + require("assets/img/clouds.png").default + ")",
           }}
         />
         <h6 className="category category-absolute">
