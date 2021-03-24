068c67fe254cf4ac701a5539b3ff5e2cda993288
EINazare <emanuelioannazare@gmail.com>
2021-03-24 10:31:46 +0200

    style: run prettier

--- src/views/docs/Sections/getting-started/BuildTools.js
+++ src/views/docs/Sections/getting-started/BuildTools.js
@@ -99,7 +99,9 @@ class BuildTools extends React.Component {
                 </td>
                 <td className="text-left">
                   This will create a build directory with a production build of
-                  your app. (NOTE!!! you will have to delete or configure to your specs the <code>homepage</code> from inside our <code>package.json</code> file)
+                  your app. (NOTE!!! you will have to delete or configure to
+                  your specs the <code>homepage</code> from inside our{" "}
+                  <code>package.json</code> file)
                 </td>
                 <td className="text-right">
                   <a
