f111a0646c5c6751e178f25b6147314aa49eb253
Alan-Figueredo <afigueredo2000@gmail.com>
2023-01-30 22:25:30 -0300

    cambios footer

--- src/components/Footers/DemoFooter.js
+++ src/components/Footers/DemoFooter.js
@@ -20,47 +20,64 @@
 import React from "react";
 
 // reactstrap components
-import { Row, Container } from "reactstrap";
+import { Row, Container, Col } from "reactstrap";
+import "./Footer.css"
+import { FaLinkedinIn, FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa"
 
 function DemoFooter() {
   return (
     <footer className="footer footer-black footer-white">
       <Container>
+        <Row className="mb-4">
+          <Col>
+            <img src={""} alt="logo prepaid" />
+          </Col>
+          <Col >
+            <h3 className="mb-3" ><b>Quienes somos</b></h3>
+            <a href="#"><b>Nosotros</b></a>
+            <br />
+            <a href="#"><b>De dónde venimos</b></a>
+            <br />
+            <a href="#"><b>Grupo Asociativo</b></a>
+            <br />
+            <a href="#"><b>Cooperativismo</b></a>
+            <br />
+            <a href="#"><b>Trabajá con nosotros</b></a>
+          </Col>
+          <Col >
+            <h3 className="mb-3"><b>Planes</b></h3>
+            <a href="#"><b>Cartillas</b></a>
+            <br />
+            <a href="#"><b>Centros de atención</b></a>
+            <br />
+            <a href="#"><b>Tu cuenta online</b></a>
+          </Col>
+          <Col >
+            <h3 className="mb-3"><b>Información util</b></h3>
+            <a href="#"><b>Emergencias</b></a>
+            <br />
+            <a href="#"><b>Asistencia al viajero</b></a>
+            <br />
+            <a href="#"><b>Información administrativa</b></a>
+            <br />
+            <a href="#"><b>Preguntas frecuentes</b></a>
+            <br />
+            <a href="#"><b>Reglamento general</b></a>
+            <br />
+            <a href="#"><b>Bajas</b></a>
+            <br />
+            <a href="#"><b>Tramite de arrepentimiento</b></a>
+          </Col>
+          <Col className="flex flex-col">
+            <h3><b>Novedades</b></h3>
+            <h3><b>Contacto</b></h3>
+            <h3><FaLinkedinIn className="redes"  /><FaInstagram className="redes" /><FaFacebookF className="redes" /><FaTwitter className="redes" /></h3>
+          </Col>
+        </Row>
         <Row>
-          <nav className="footer-nav">
-            <ul>
-              <li>
-                <a
-                  href="https://www.creative-tim.com?ref=pkr-footer"
-                  target="_blank"
-                >
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://blog.creative-tim.com/?ref=pkr-footer"
-                  target="_blank"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.creative-tim.com/license?ref=pkr-footer"
-                  target="_blank"
-                >
-                  Licenses
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="credits ml-auto">
-            <span className="copyright">
-              © {new Date().getFullYear()}, made with{" "}
-              <i className="fa fa-heart heart" /> by Creative Tim
-            </span>
-          </div>
+          <Col md={12}>
+            <p>Superintendencia de Servicios de Salud Órgano de Control de Obras Sociales y Entidades de Medicina Prepaga www.salud.gov.ar - RNEMP N° 1194 (provisorio) </p>
+          </Col>
         </Row>
       </Container>
     </footer>
