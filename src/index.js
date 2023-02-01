2bdcdedd9a86afb2af1daf7376e5235e0b725473
Alan-Figueredo <afigueredo2000@gmail.com>
2023-02-01 14:56:28 -0300

    vista de planes, link de planes en el navbar

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import NucleoIcons from "vistas/NucleoIcons.js";
 import LandingPage from "vistas/LandingPage.js";
 import ProfilePage from "vistas/ProfilePage.js";
 import RegisterPage from "vistas/RegisterPage.js";
+import Planes from "vistas/Planes/Planes";
 
 // others
 
@@ -53,6 +54,10 @@ root.render(
       <Route
         path="/register-page"
         render={(props) => <RegisterPage {...props} />}
+      />
+      <Route
+        path="/planes"
+        render={(props) => <Planes {...props} />}
       />
       <Redirect to="/index" />
     </Switch>
---
+++ src/vistas/Planes/Planes.js
@@ -0,0 +1,95 @@
+import Footer from "componentes/Footer/Footer";
+import NavBar from "componentes/Navbar/Navbar";
+import "./Planes.css"
+import { Button, Col, Container, Row } from "reactstrap";
+
+const Planes = () => {
+    return (
+        <>
+            <NavBar />
+            <main id="planes">
+                <div
+                    style={{
+                        backgroundImage: "url(" + require("assets/img/Feliz-3.jpg") + ")", backgroundSize: "cover", minHeight: "600px", backgroundPosition: "center"
+                    }}
+                    data-parallax={true}
+                >
+
+                </div>
+                <Container>
+
+                    <Row className="w-100">
+                        <Col>
+                            <img src={require("assets/img/plan1.png")} alt="imagen de plan" className="img-fluid" style={{ marginTop: "-40px" }} />
+                            <h2>Plan Cerca</h2>
+                            <h5>Un plan práctico y accesible que te acerca a lo que necesitas.</h5>
+                            <p>
+                                Una opción simple, ágil y nativamente digital, que cuenta con una cartilla de prestadores seleccionada por proximidad e incluye un set de servicios digitales como credencial digital, e-doc, app, cartilla online y Ava, tu asistente virtual.
+                            </p>
+                        </Col>
+                        <Col>
+                            <img src={require("assets/img/plan2.png")} alt="imagen de plan" className="img-fluid" style={{ marginTop: "-40px" }} />
+                            <h2>Plan Cerca</h2>
+                            <h5>Un plan práctico y accesible que te acerca a lo que necesitas.</h5>
+                            <p>
+                                Nuestro Plan Integral cuenta con una amplia cartilla de prestadores de alcance nacional y una importante combinación de coberturas a precios accesibles, a través de la aplicación de copagos.
+                            </p>
+                        </Col>
+                        <Col>
+                            <img src={require("assets/img/plan3.png")} alt="imagen de plan" className="img-fluid" style={{ marginTop: "-40px" }} />
+                            <h2>Plan Cerca</h2>
+                            <h5>Un plan práctico y accesible que te acerca a lo que necesitas.</h5>
+                            <p>
+                                Amplia cartilla de prestadores, internación en habitación individual con baño privado, importante descuento en farmacias adheridas, servicio de emergencias y urgencias sin cargo, y consultas a domicilio con accesibles copagos.
+                            </p>
+                        </Col>
+                        <Col>
+                            <img src={require("assets/img/plan4.png")} alt="imagen de plan" className="img-fluid" style={{ marginTop: "-40px" }} />
+                            <h2>Plan Cerca</h2>
+                            <h5>Un plan práctico y accesible que te acerca a lo que necesitas.</h5>
+                            <p>
+                                Agilidad y máxima calidad de servicios con detalles de confort. Amplia cartilla de prestadores y centros médicos de prestigio, internación en suite y consultas a domicilio y servicio de emergencias y urgencias sin cargo.
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button className="my-4">Saber Más</Button>
+                        </Col>
+                        <Col>
+                            <Button className="my-4">Saber Más</Button>
+                        </Col>
+                        <Col>
+                            <Button className="my-4">Saber Más</Button>
+                        </Col>
+                        <Col>
+                            <Button className="my-4">Saber Más</Button>
+                        </Col>
+                    </Row>
+                    <Row className="w-100 mt-5">
+                        <Col md={6}>
+                            <p>
+                                <b>En Avalian pensamos y actuamos en función de nuestros asociados.
+                                    Con nuestra amplia gama de Planes Médicos procuramos un modelo de atención en su concepto más amplio, apuntando claramente a la promoción de la salud, educando en la prevención y auspiciando conductas saludables para disminuir los factores de riesgo.
+                                </b>
+                            </p>
+                        </Col>
+                        <Col md={6}>
+                            <img src={require("assets/img/planes.png")} alt="tarjetas de planes" className="img-fluid" />
+                        </Col>
+                    </Row>
+                    <Row className=" mb-5 ">
+                        <Col md={6}>
+                            <img src={require("assets/img/consultorioPaciente.jpg")} alt="tarjetas de planes" style={{ minHeight: "200px", height: "300px", width: "inherit" }} />
+                        </Col>
+                        <Col md={6}>
+                            <img src={require("assets/img/consultorioVacio.jpg")} alt="tarjetas de planes" style={{ minHeight: "200px", height: "300px", width: "inherit" }} />
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    )
+}
+export default Planes;
