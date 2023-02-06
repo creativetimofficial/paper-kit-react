1e9fbf1934a890a26e7d180e422c9e2fdaa9c429
Kevin Figueredo <74889406+kevinfigueredo2000@users.noreply.github.com>
2023-02-06 02:42:05 -0300

    Linkeo a "Cotiza" desde "Contratá"

--- src/vistas/Contrata/Contrata.jsx
+++ src/vistas/Contrata/Contrata.jsx
@@ -4,6 +4,7 @@ import { createRef } from "react";
 import { Button, Card, Col, Container, Row } from "reactstrap"
 import { MdOutlineAddIcCall } from "react-icons/md"
 import { RiCustomerService2Fill, RiQuestionnaireFill } from "react-icons/ri"
+import { Link } from "react-router-dom";
 
 
 export const Contrata = () => {
@@ -26,9 +27,11 @@ export const Contrata = () => {
                         <h2>Además accedés<br />
                             a un 45% de ahorro.</h2>
                         <br />
-                        <Button className="btn-round" color="neutral" type="button" outline>
-                            Continuar
-                        </Button>
+                        <Link to="/cotiza">
+                            <Button className="btn-round" color="neutral" type="button" outline>
+                                Continuar
+                            </Button>
+                        </Link>
                     </div>
                 </Container>
             </div>
@@ -89,17 +92,17 @@ export const Contrata = () => {
                 <Container className="py-4">
                     <Row>
                         <Col md="3" className="mx-auto">
-                            <MdOutlineAddIcCall className="my-2" style={{ fontSize: "5vh",border: "2px solid #f33816", borderRadius: "50%", padding:"10px", backgroundColor:"white"}}/>
+                            <MdOutlineAddIcCall className="my-2" style={{ fontSize: "5vh", border: "2px solid #f33816", borderRadius: "50%", padding: "10px", backgroundColor: "white" }} />
                             <p className="my-2">EMERGENCIAS AL</p>
                             <h3 className="my-2">0800 555 5556</h3>
                         </Col>
                         <Col md="3" className="mx-auto">
-                            <RiCustomerService2Fill className="my-2" style={{ fontSize: "5vh",border: "2px solid #f33816", borderRadius: "50%", padding:"10px", backgroundColor:"white"}}/>
+                            <RiCustomerService2Fill className="my-2" style={{ fontSize: "5vh", border: "2px solid #f33816", borderRadius: "50%", padding: "10px", backgroundColor: "white" }} />
                             <p className="my-2">ATENCIÓN TELEFÓNICA </p>
                             <h3 className="my-2">0810 222 SALUD (72583)</h3>
                         </Col>
                         <Col md="3" className="mx-auto">
-                            <RiQuestionnaireFill className="my-2" style={{ fontSize: "5vh",border: "2px solid #f33816", borderRadius: "50%", padding:"10px", backgroundColor:"white"}}/>
+                            <RiQuestionnaireFill className="my-2" style={{ fontSize: "5vh", border: "2px solid #f33816", borderRadius: "50%", padding: "10px", backgroundColor: "white" }} />
                             <p className="my-2">PREGUNTAS FRECUENTES </p>
                             <h3 className="my-2">Despejá tus dudas</h3>
                         </Col>
